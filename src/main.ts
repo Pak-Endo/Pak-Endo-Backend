8ad21750a924218cfb4a94f6f7d06f51182d7a68
Mohid123 <muhammadmohid141@gmail.com>
2023-08-01 09:15:22 +0500

    swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
+  app.setGlobalPrefix('api');
+
+  app.use(bodyParser.json({ limit: '500mb' }));
+
+  app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Event-Manager')
+    .setDescription('Event-Manager APIs')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('Event-Manager APIs')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
