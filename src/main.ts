ad66cc65909dcab218c52a8c1ff1bae4b87492f5
Mohid123 <muhammadmohid141@gmail.com>
2023-08-01 09:45:31 +0500

    handlebars basic config and setup

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import { join } from 'path';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
-  app.setGlobalPrefix('api');
+  const app = await NestFactory.create<NestExpressApplication | any>(AppModule);
+  // app.setGlobalPrefix('api');
 
   app.use(bodyParser.json({ limit: '500mb' }));
 
   app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
 
   app.enableCors({
     origin: '*',
