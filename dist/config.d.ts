9693262ef7ba6ea847a7a58a727580e6a3b90726
Mohid123 <muhammadmohid141@gmail.com>
2023-08-18 15:55:25 +0500

    Latest build with config changes 8/18/2023

--- dist/config.d.ts
+++ dist/config.d.ts
@@ -1,4 +1,9 @@
 declare const _default: {
+    MAIL_HOST: string;
+    MAIL_PORT: number;
+    MAIL_USER: string;
+    MAIL_PASSWORD: string;
+    MAIL_FROM: string;
     mongoURI: string;
     SECRET_KEY: string;
     URL: string;
--- dist/config.js
+++ dist/config.js
@@ -1,9 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    mongoURI: 'mongodb://127.0.0.1:27017/Event-Manager',
-    SECRET_KEY: 'MySuperSecretKeyIsSuperSecret',
-    URL: 'http://localhost:3000/api',
+    MAIL_HOST: "sandbox.smtp.mailtrap.io",
+    MAIL_PORT: 2525,
+    MAIL_USER: "a0107a64f648db",
+    MAIL_PASSWORD: "4f7b8588c4f5ea",
+    MAIL_FROM: "noreply",
+    mongoURI: "mongodb+srv://fahadulabideenqt:gdomrpovujxxbdul@pakendodb.4tan5v5.mongodb.net/?retryWrites=true&w=majority",
+    SECRET_KEY: "MySuperSecretKeyIsSuperSecret",
+    URL: "http://quaidtech.ddns.net:8200/api",
     whiteListedExtensions: ['.apng', '.avif', '.gif', '.jpg', '.jpeg', '.jfif', '.pjpeg', '.pjp', '.png', '.svg', '.webm', '.mkv', '.flv', '.vob', '.ogg', '.ogv', '.gif', '.mov', '.wmv', '.3gp', '.mp4', '.m4p', '.m4v']
 };
 //# sourceMappingURL=config.js.map
