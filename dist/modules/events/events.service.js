959080b80566cc44791e0a9de7278f0ccbea5df0
Mohid123 <muhammadmohid141@gmail.com>
2023-08-23 15:43:04 +0500

    new build

--- dist/modules/events/events.service.js
+++ dist/modules/events/events.service.js
@@ -48,10 +48,10 @@ let EventsService = exports.EventsService = class EventsService {
             filters = { ...filters, type: query };
         }
         if (startDate) {
-            filters = { ...filters, startDate: { $gte: startDate } };
+            filters = { ...filters, startDate: { $gte: Number(startDate) } };
         }
         if (endDate) {
-            filters = { ...filters, endDate: { $lte: endDate } };
+            filters = { ...filters, endDate: { $lte: Number(endDate) } };
         }
         if (speaker) {
             const query = new RegExp(`${speaker}`, 'i');
