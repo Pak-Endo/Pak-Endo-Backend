17d67dcafc1a488735706f75fc7004991735f99d
Mohid123 <muhammadmohid141@gmail.com>
2023-09-21 14:00:48 +0500

    venue crud added to app-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { FavoritesModule } from './modules/favorites/favorites.module';
 import { AttendedModule } from './modules/attended/attended.module';
 import { SpeakersModule } from './modules/speakers/speakers.module';
 import { SponsorsModule } from './modules/sponsors/sponsors.module';
+import { VenuesModule } from './modules/venues/venues.module';
 
 @Module({
   imports: [
@@ -31,7 +32,8 @@ import { SponsorsModule } from './modules/sponsors/sponsors.module';
     MediaUploadModule,
     CronjobsModule,
     SpeakersModule,
-    SponsorsModule
+    SponsorsModule,
+    VenuesModule
   ],
   controllers: [AppController],
   providers: [AppService],
