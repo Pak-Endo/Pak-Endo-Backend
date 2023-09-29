e9d87ef2a6beed6c409657a70f2ca4b6e695acb4
Mohid123 <muhammadmohid141@gmail.com>
2023-09-29 15:58:52 +0500

    remove

--- dist/dto/sponsor.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class SponsorDto {
-    _id: string;
-    sponsorName: string;
-    sponsorLogo: string;
-    uniqueID: string;
-    description: string;
-    deletedCheck: boolean;
-    contact: string;
-}
--- dist/modules/cronjobs/cronjobs.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Model } from 'mongoose';
-import { Event } from 'src/schemas/events.schema';
-export declare class CronjobsService {
-    private readonly eventModel;
-    constructor(eventModel: Model<Event>);
-    startCronJobForEventStatus(): Promise<void>;
-    endCronJobForEventStatus(): Promise<void>;
-}
