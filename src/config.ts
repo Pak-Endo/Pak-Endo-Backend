c53d479dd304fdb9a73774dadac07a3063c6ba4f
Mohid123 <muhammadmohid141@gmail.com>
2023-10-04 15:48:55 +0500

    fixes

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 export default { 
   mongoURI: 'mongodb://127.0.0.1:27017/Event-Manager',
   SECRET_KEY: 'MySuperSecretKeyIsSuperSecret',
-  URL:'https://pakEndo.animetography-blog.com/api',
-  PORT:3600,
+  URL:'http://localhost:3000/api',
+  PORT:3000,
   whiteListedExtensions: ['.apng', '.avif', '.gif', '.jpg', '.jpeg', '.jfif', '.pjpeg', '.pjp', '.png', '.svg','.webm', '.mkv', '.flv', '.vob', '.ogg', '.ogv', '.gif', '.mov', '.wmv', '.3gp', '.mp4', '.m4p', '.m4v', '.mp3']
 }
