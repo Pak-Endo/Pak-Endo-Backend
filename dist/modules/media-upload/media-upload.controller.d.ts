17361b029ba804704f3cdbf936028132a73856d6
Mohid123 <muhammadmohid141@gmail.com>
2023-10-17 12:03:07 +0500

    remove dist from gitignore

--- dist/modules/media-upload/media-upload.controller.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class MediaUploadController {
-    uploadMedia(file: any, folderName: string, req: any): Promise<any>;
-    mediaFiles(folderName: string, fileName: string, res: any, req: any, size?: string): Promise<any>;
-}
