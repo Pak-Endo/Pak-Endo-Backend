5a63c7ed56f8be3f39d58b6e942b32ee6c74801f
Mohid123 <muhammadmohid141@gmail.com>
2023-11-09 13:00:54 +0500

    changed config

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 /* eslint-disable prettier/prettier */
 export default { 
-  mongoURI: 'mongodb://127.0.0.1:27017/Event-Manager',
+  mongoURI: 'mongodb+srv://mohidqt:mXoErGxOClDkebd8@cluster0.froawy0.mongodb.net/',
   SECRET_KEY: 'MySuperSecretKeyIsSuperSecret',
-  URL:'https://pakEndo.animetography-blog.com/api',
+  URL:'https://pakendo.quaidtech.net/api',
   PORT:3600,
   whiteListedExtensions: ['.apng', '.avif', '.gif', '.jpg', '.jpeg', '.jfif', '.pjpeg', '.pjp', '.png', '.svg','.webm', '.mkv', '.flv', '.vob', '.ogg', '.ogv', '.gif', '.mov', '.wmv', '.3gp', '.mp4', '.m4p', '.m4v', '.mp3', '.pdf']
 }
